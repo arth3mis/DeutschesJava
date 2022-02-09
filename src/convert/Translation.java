727234ffd508ba932b013249ee958c1080f0b279
jannis <j.mueller@polyas.de>
2022-02-09 22:35:52 +0100

    20: Able to read the translation .txt files

--- src/convert/Translation.java
+++ src/convert/Translation.java
@@ -4,7 +4,34 @@ import java.util.HashMap;
 
 public class Translation {
 
-    String englishWord;
-    HashMap<String, Translation> staticTranslations;
+    private String translationText;
+    private HashMap<String, Translation> staticTranslations = new HashMap<>();
 
+
+    Translation() {
+        translationText = null;
+    }
+
+    Translation(String translationText) {
+        this.translationText = translationText;
+    }
+
+    public void print(String preText) {
+        staticTranslations.forEach((k, v) -> {
+            System.out.println(preText + k + " -> " + v.translationText);
+            v.print(preText + "\t");
+        });
+    }
+
+    public String getTranslationText() {
+        return translationText;
+    }
+
+    public HashMap<String, Translation> getStaticTranslations() {
+        return staticTranslations;
+    }
+
+    public void setStaticTranslations(HashMap<String, Translation> staticTranslations) {
+        this.staticTranslations = staticTranslations;
+    }
 }
